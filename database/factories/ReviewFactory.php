7b49e490bb79fd952218a50b77c85985f3416067
imad <imdalikhan@gmail.com>
2021-02-02 12:05:29 +0530

    relationship added

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,9 +2,8 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model\Review;
 use App\Model\Product;
-
+use App\Model\Review;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
