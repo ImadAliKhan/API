598ee4734f04779b640966926019cea189a8f8a1
imad <imdalikhan@gmail.com>
2021-02-03 23:51:00 +0530

    till update api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +43,19 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+         $post= $request->all();
+         unset($post['description']);
+        $post['detail']=$request->description;
+        $post['user_id']=Auth::user()->id;
+         // print_r($post);
+
+        $product = Product::create($post);
+
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
@@ -72,7 +90,17 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+         $post= $request->all();
+         unset($post['description']);
+        $post['detail']=$request->description;
+
+        $product->update($post);
+
+        
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
+
     }
 
     /**
